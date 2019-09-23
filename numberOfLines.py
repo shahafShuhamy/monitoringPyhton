cc32c4b82b5468378b846a877e42b4379dcd80d7
shahafShuhamy <Shahaf761@gmail.com>
2019-09-23 18:08:10 +0300

    fix decoding issues

--- numberOfLines.py
+++ numberOfLines.py
@@ -2,7 +2,7 @@ import os
 rootdir = os.path.dirname(os.path.realpath(__file__))
 
 def file_len(fname):
-    with open(fname) as f:
+    with open(fname, 'r', encoding='utf8', errors='ignore') as f:
         i = 0
         for i, l in enumerate(f):
             pass
@@ -11,7 +11,6 @@ def file_len(fname):
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
-        #print os.path.join(subdir, file)
         filepath = subdir + os.sep + file
         print (filepath)
         print(file_len(filepath))
